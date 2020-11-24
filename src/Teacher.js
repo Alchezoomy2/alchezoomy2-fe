26aebc673213a550be5f7caf41cad6ff0a7f548d
Paul Stevens <twinkisan@hotmail.com>
2020-11-24 10:40:13 -0800

    Teacher.js takes in userData, sets it to App.js state

--- src/Teacher.js
+++ src/Teacher.js
@@ -4,19 +4,24 @@ import fetch from 'superagent';
 export default class teacher extends Component {
     state = {
         loading: true,
-        returnedObject: {}
     }
 
     componentDidMount = async () => {
         const URL = 'https://alchezoomy2.herokuapp.com/oauth';
         try {
-            const returnedObject = await fetch.post(URL).send({ token: this.props.code });
+            const returnedObject = await fetch
+                .post(URL)
+                .send({ token: this.props.code });
+
+            const userInfo = returnedObject.body;
+
+            this.props.handleSetState(userInfo);
             this.setState({
-                returnedObject: returnedObject.body,
                 loading: false
             })
 
-            console.log(returnedObject)
+            this.props.handleSetState({ token: this.returnedToken.body });
+
         } catch (e) {
             throw e;
         }
@@ -29,7 +34,7 @@ export default class teacher extends Component {
                     ? <p>loading:  {this.props.code}</p>
                     : <div>
                         <p>{this.props.code}</p>
-                        <p>{this.state.returnedObject}git</p>
+                        <p>{this.props.appState.appState}</p>
                     </div>
                 }
             </div>
