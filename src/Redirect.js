b3ad8a252374fbcf5ef6b1f43cdbbd5834388e9e
Paul Stevens <twinkisan@hotmail.com>
2020-12-04 17:58:10 -0800

    debugging redirect

--- src/Redirect.js
+++ src/Redirect.js
@@ -6,7 +6,7 @@ export default class Redirect extends Component {
         let code = new URLSearchParams(window.location.search);
 
         this.props.handleSetState({ code: code.get('code') });
-
+        alert(this.props.user_type)
         if (this.props.user_type === 'teacher') {
             this.props.history.push('/teacher');
         } else {
