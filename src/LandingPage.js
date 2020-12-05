128a22c9e87a79f1eb81e7a7c6d7591ca8515596
Paul Stevens <twinkisan@hotmail.com>
2020-12-04 17:50:19 -0800

    changing up redirect

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -6,7 +6,7 @@ export default class LandingPage extends Component {
 
     handleButtonPress = async (buttonPressed) => {
         this.props.handleSetState({ "user_type": buttonPressed });
-        this.props.history.push("https://zoom.us/oauth/authorize?response_type=code&client_id=AxrbH83_Q0aEO273dFIafw&redirect_uri=https://alchezoomy2.netlify.app/redirect");
+        window.location.href = "https://zoom.us/oauth/authorize?response_type=code&client_id=AxrbH83_Q0aEO273dFIafw&redirect_uri=https://alchezoomy2.netlify.app/redirect";
     }
 
 
