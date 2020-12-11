03d1d23d930511731b561f468b6c58ed6cd0c298
Paul Stevens <twinkisan@hotmail.com>
2020-12-11 11:43:54 -0800

    debugging

--- src/Redirect.js
+++ src/Redirect.js
@@ -24,7 +24,7 @@ export const AutoRedirect = (props) => {
         // <p>REDIRECTING
         // autoRedirect()
         // </p>
-        <p>{userType}</p>
+        <p>{store.userType}</p>
     )
 
 }
