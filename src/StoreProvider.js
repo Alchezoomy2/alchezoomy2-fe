48cde3dc33554354d7c25f8c6b7cd9088be95146
Paul Stevens <twinkisan@hotmail.com>
2020-12-10 22:37:50 -0800

    what a difference an r makes

--- src/StoreProvider.js
+++ src/StoreProvider.js
@@ -34,4 +34,4 @@ export const StoreProvider = ({ children }) => {
     )
 };
 
-export const userStateStore = () => useContext(StoreContext);
+export const useStateStore = () => useContext(StoreContext);
