602ca6737045e1fe29f0ec22ee6cd6d9ca4c6ace
Paul Stevens <twinkisan@hotmail.com>
2020-12-11 09:51:46 -0800

    debugging

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,21 +1,21 @@
 import { useObserver } from 'mobx-react';
 import React from 'react';
-// import { useStateStore } from "./StoreProvider"
+import { useStateStore } from "./StoreProvider"
 
 
 
 export const Redirect = (props) => {
-    // const store = useStateStore();
-    // const [userType] = React.useState("");
+    const store = useStateStore();
+    const [userType] = React.useState("");
 
-    // let code = new URLSearchParams(this.props.location.search);
-    // store.changeCode(code);
+    let code = new URLSearchParams(this.props.location.search);
+    store.changeCode(code);
 
-    // if (userType === 'teacher') {
-    //     this.props.history.push = '/teacher';
-    // } else {
-    //     this.props.history.push = '/student';
-    // }
+    if (userType === 'teacher') {
+        this.props.history.push = '/teacher';
+    } else {
+        this.props.history.push = '/student';
+    }
 
 
     return useObserver(() => {
