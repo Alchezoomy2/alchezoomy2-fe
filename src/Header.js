7ba1fbd4d0b42de6995d73cc832cf514ea4f00e9
Paul Stevens <twinkisan@hotmail.com>
2020-12-11 14:32:50 -0800

    debugging

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/MenuIcon';
 import { useStateStore } from './StoreProvider.js'
 import { useObserver } from 'mobx-react';
 import classes from '*.module.css';
--- src/Redirect.js
+++ src/Redirect.js
@@ -1,23 +1,23 @@
 import { useObserver } from 'mobx-react';
 import React from 'react';
 import { useStateStore } from "./StoreProvider"
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 
 
 
 
 export const AutoRedirect = (props) => {
     const store = useStateStore();
-    const history = useHistory();
+    // const history = useHistory();
 
     let code = new URLSearchParams(props.location.search);
     store.changeCode(code.get('code'));
 
-    if (store.userType === 'teacher') {
-        history.push('/teacher');
-    } else {
-        history.push('/student');
-    }
+    // if (store.userType === 'teacher') {
+    //     history.push('/teacher');
+    // } else {
+    //     history.push('/student');
+    // }
 
 
     return useObserver(() =>
