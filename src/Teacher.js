d6f33dda60c0b5ff7a2beccff48ccc09f3a53186
Paul Stevens <twinkisan@hotmail.com>
2020-12-11 17:52:22 -0800

    debugging

--- src/Teacher.js
+++ src/Teacher.js
@@ -9,43 +9,54 @@ import { Link } from "react-router-dom";
 export const Teacher = () => {
     const store = useStateStore();
 
+
+    async function retrieveTeacherInfo() {
+        const returnedObject = await fetch
+            .post(store.serverUrl + '/teacher/oauth')
+            .send({ code: store.code });
+
+        store.changeTeacherInfo(returnedObject.body);
+        console.log(store.teacherInfo.new_user)
+        console.log('first')
+    }
+
+    async function newTeacher() {
+        return await fetch
+            .post(store.serverUrl + '/teacher/new')
+            .send({ teacher_info: store.teacherInfo });
+    }
+
+
     useEffect(() => {
-        async function retrieveTeacherInfo() {
-            const returnedObject = await fetch
-                .post(store.serverUrl + '/teacher/oauth')
-                .send({ code: store.code });
 
-            store.changeTeacherInfo(returnedObject.body);
-            console.log(store.teacherInfo.new_user)
-            console.log('first')
-        }
+
         retrieveTeacherInfo();
-
+        newTeacher();
     });
 
-    useEffect(() => {
-        console.log(store.teacherInfo.new_user)
-        console.log('second')
-        async function newTeacher() {
-            return await fetch
-                .post(store.serverUrl + '/teacher/new')
-                .send({ teacher_info: store.teacherInfo });
-        }
+    // useEffect(() => {
+    //     console.log(store.teacherInfo.new_user)
+    //     console.log('second')
+    //     async function newTeacher() {
+    //         return await fetch
+    //             .post(store.serverUrl + '/teacher/new')
+    //             .send({ teacher_info: store.teacherInfo });
+    //     }
 
-        async function exisitingTeacher() {
-            return await fetch
-                .post(store.serverUrl + '/teacher/meetings')
-                .send({ teacher_info: store.teacherInfo });
-        }
+    //     async function exisitingTeacher() {
+    //         return await fetch
+    //             .post(store.serverUrl + '/teacher/meetings')
+    //             .send({ teacher_info: store.teacherInfo });
+    //     }
 
-        if (store.teacherInfo.new_user) {
-            store.changeMeetingsObj(newTeacher());
-        } else {
-            store.changeMeetingsObj(exisitingTeacher())
-        }
-        console.log(store.meetingsObj);
-        console.log('third')
-    })
+    //     if (store.teacherInfo.new_user) {
+    //         store.changeMeetingsObj(newTeacher());
+    //     } else {
+    //         store.changeMeetingsObj(exisitingTeacher())
+    //     }
+    //     console.log(store.meetingsObj);
+    //     console.log('third')
+    // })
 
 
 
