a86601b04af481a377833c07317c5ffe04b4948e
Paul Stevens <twinkisan@hotmail.com>
2020-12-11 17:08:37 -0800

    debugging

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,23 +1,21 @@
 import { useObserver } from 'mobx-react';
 import React from 'react';
 import { useStateStore } from "./StoreProvider"
-// import { useHistory } from "react-router-dom";
-
-
+import { useHistory } from "react-router-dom";
 
 
 export const AutoRedirect = (props) => {
     const store = useStateStore();
-    // const history = useHistory();
+    const history = useHistory();
     console.log(store)
-    // let code = new URLSearchParams(props.location.search);
-    // store.changeCode(code.get('code'));
+    let code = new URLSearchParams(props.location.search);
+    store.changeCode(code.get('code'));
 
-    // if (store.userType === 'teacher') {
-    //     history.push('/teacher');
-    // } else {
-    //     history.push('/student');
-    // }
+    if (store.userType === 'teacher') {
+        history.push('/teacher');
+    } else {
+        history.push('/student');
+    }
 
 
     return useObserver(() =>
