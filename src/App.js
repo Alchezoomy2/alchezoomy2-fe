7b278777bef61cae4307cac9695229fdc3fb252c
Paul Stevens <twinkisan@hotmail.com>
2020-12-11 16:51:18 -0800

    debugging

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 import LandingPage from './LandingPage.js';
 import AutoRedirect from './Redirect.js'
 import Teacher from './Teacher.js';
-import Header from './Header.js';
+// import Header from './Header.js';
 
 
 function App() {
