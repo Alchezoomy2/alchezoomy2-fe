ea94c5a8b98b7e1f8afbcac8584e903596fdfa7b
Paul Stevens <twinkisan@hotmail.com>
2020-12-13 10:59:10 -0800

    debugging

--- src/Teacher.js
+++ src/Teacher.js
@@ -4,6 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Paper, List, ListItem, ListItemText, Chip } from '@material-ui/core';
 import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 import VideoLabelIcon from '@material-ui/icons/VideoLabel';
+import ChatIcon from '@material-ui/icons/Chat';
+import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
 import fetch from 'superagent';
 import { useObserver } from 'mobx-react';
 
@@ -50,40 +52,40 @@ export const Teacher = () => {
     useEffect(() => {
         return retrieveTeacherInfo()
             .then(retrieveTeacher)
-    });
-    console.log((store.meetingsObj));
+            .then(console.log((store.meetingsObj[0])))            )
+});
 
 
-    return useObserver(() =>
-        <Paper elevation={3} >
-            <List className={classes.root}>
-                {store.meetingsObj ?
-                    store.meetingsObj.map(meeting =>
-                        <ListItem alignItems="flex-start">
-                            <ListItemText
-                                primary={meeting.start_time}
-                                secondary={meeting.topic} />
+return useObserver(() =>
+    <Paper elevation={3} >
+        <List className={classes.root}>
+            {store.meetingsObj ?
+                store.meetingsObj.map(meeting =>
+                    <ListItem alignItems="flex-start">
+                        <ListItemText
+                            primary={meeting.start_time}
+                            secondary={meeting.topic} />
                         if (meeting.video_url) <Chip size="small" icon={<VideoLabelIcon />} label="video" />
                         if (meeting.audio_url) <Chip size="small" icon={<VolumeUpIcon />} label="audio" />
-                        if (meeting.chat_url) <Chip size="small" icon={<VolumeUpIcon />} label="chat" />
-                        if (meeting.transcript_url) <Chip size="small" icon={<VolumeUpIcon />} label="transcript" />
+                        if (meeting.chat_url) <Chip size="small" icon={<ChatIcon />} label="chat" />
+                        if (meeting.transcript_url) <Chip size="small" icon={<RecordVoiceOverIcon />} label="transcript" />
 
 
-                        </ListItem>
+                    </ListItem>
 
-                    )
-                    :
-                    <p>LOADING!</p>
-                }
-
-
-            </List>
+                )
+                :
+                <p>LOADING!</p>
+            }
 
 
+        </List>
 
-        </Paper>
 
-    )
+
+    </Paper>
+
+)
 }
 
 export default Teacher;
