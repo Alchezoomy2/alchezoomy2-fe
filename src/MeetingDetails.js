4a147cdfc3feba4ecd195b1a7828eb08d090b81d
Paul Stevens <twinkisan@hotmail.com>
2020-12-17 11:56:03 -0800

    debugging

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -1,64 +1,47 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 // import ReactPlater from "react-player";
 import { useObserver } from 'mobx-react';
 import { useStateStore } from './StoreProvider.js'
-// import fetch from 'superagent';
+import fetch from 'superagent';
 
-// const findMeetingObj = (meetingId, meetingsObj) => {
-//     for (let meeting of meetingsObj) {
-//         console.log(meeting)
-//         if (meeting.id === meetingId) return meeting;
-//     }
-// }
+const findMeetingObj = (meetingId, meetingsObj) => {
+    for (let meeting of meetingsObj) {
+        if (meeting.id === meetingId) return meeting;
+    }
+}
 
 
 export const MeetingDetails = (props) => {
     const store = useStateStore();
-    // let [loading] = useState(true)
-    // let meetingId = useRef(props.match.params.id)
-    // let chatArray = useRef();
-    // let transcriptArray = useRef();
+    let [loading, setLoading] = useState(true)
+    let meetingId = useRef(props.match.params.id)
+    let chatArray = useRef();
+    let transcriptArray = useRef();
+    let meetingObj = findMeetingObj(meetingId, store.meetingsObj)
 
 
-    // useEffect(() => {
+    console.log('------------------------------------');
+    console.log(`meetingObj:  ${meetingObj}`);
+    console.log('------------------------------------');
+    useEffect(() => {
 
-    //     // async function fetchMeetingDetails(meetingId) {
-    //     //     const returnedObject = await fetch
-    //     //         .get(store.serverUrl + `/teacher/meetings/${meetingId.current}`)
+        const fetchMeetingDetails = async (meetingId) => {
+            const returnedObject = await fetch
+                .get(store.serverUrl + `/teacher/meetings/${meetingId.current}`)
 
-    //     //     chatArray.current = await returnedObject.body.chat;
+            chatArray.current = await returnedObject.body.chat;
 
-    //     //     transcriptArray.current = await returnedObject.body.transcript;
+            transcriptArray.current = await returnedObject.body.transcript;
 
-    //     //     setLoading(false);
+            setLoading(false);
+        }
 
-    //     //     console.log('------------------------------------');
-    //     //     console.log(`chatArray[0]:  ${chatArray[0]}`);
-    //     //     console.log('------------------------------------');
-
-    //     //     console.log('------------------------------------');
-    //     //     console.log(`loading:  ${loading}`);
-    //     //     console.log('------------------------------------');
-    //     // }
-
-    //     const meetingsObj = store.meetingsObj;
-    //     console.log(meetingsObj)
-
-    //     let meetingObj = findMeetingObj(meetingId, meetingsObj)
-
-
-    //     console.log('------------------------------------');
-    //     console.log(`meetingObj:  ${meetingObj}`);
-    //     console.log('------------------------------------');
-
-
-    //     // if (meetingObj.chat_url || meetingObj.transcript_url) fetchMeetingDetails(meetingId);
-    // })
+        if (meetingObj.chat_url || meetingObj.transcript_url) fetchMeetingDetails(meetingId);
+    })
 
 
     return useObserver(() =>
-        <p>{store.meetingsObj}</p>
-
+        <p>{loading}</p>
 
 
 
