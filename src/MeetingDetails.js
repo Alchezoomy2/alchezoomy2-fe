b6a2b9812a5a432197943137c373c428b03fdc33
Paul Stevens <twinkisan@hotmail.com>
2020-12-17 11:02:26 -0800

    debugging

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 // import ReactPlater from "react-player";
 import { useObserver } from 'mobx-react';
 // import { useStateStore } from './StoreProvider.js'
@@ -8,15 +8,21 @@ import { useObserver } from 'mobx-react';
 
 export const MeetingDetails = (props) => {
     let [loading] = useState(true)
-    console.log(props.match.params.id)
-    const meetingId = props.match.params.id;
+    let [meetingId] = useRef(props.match.params.id)
 
-    console.log('------------------------------------');
-    console.log(`meetingId:  ${meetingId}`);
-    console.log('------------------------------------');
+    useEffect(() => {
+        console.log('------------------------------------');
+        console.log(`loading:  ${loading}`);
+        console.log('------------------------------------');
+        console.log('------------------------------------');
+        console.log(`meetingId.current:  ${meetingId.current}`);
+        console.log('------------------------------------');
+
+    })
+
 
     return useObserver(() =>
-        <p>{loading}</p>
+        <p>FUCK YOU!</p>
 
 
 
