ee090b1df47bba2cc6cd32ee69323646dac7112c
Paul Stevens <twinkisan@hotmail.com>
2020-12-17 10:28:50 -0800

    debugging

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import ReactPlater from "react-player";
 import { useObserver } from 'mobx-react';
 // import { useStateStore } from './StoreProvider.js'
@@ -8,54 +8,15 @@ import { useObserver } from 'mobx-react';
 
 
 export const MeetingDetails = () => {
-
-    console.log('------------------------------------');
-    console.log(`hello!`);
-    console.log('------------------------------------');
-    // const store = useStateStore();
-    // let [chatArray] = useRef();
-    // let [transcriptArray] = useRef();
-    // let [meetingId] = useRef()
-    // let [loading] = useRef(true);
-
-    // const findMeetingObj = (meetingId) => {
-    //     console.log('------------------------------------');
-    //     console.log(`meetingId:  ${meetingId}`);
-    //     console.log('------------------------------------');
-    //     console.log(store.meetingsObj)
-    //     for (let meeting in store.meetingsObj) {
-    //         if (meeting.id === meetingId) return meeting;
-    //     }
-    // }
-
-    // meetingId.current = new URLSearchParams(props.location.id);
-
-    // console.log('------------------------------------');
-    // console.log(`meetingId:  ${meetingId}`);
-    // console.log('------------------------------------');
-    // let meetingObj = findMeetingObj(meetingId);
-    // console.log(meetingObj)
-
-    // useEffect(() => {
-
-    //     async function fetchMeetingDetails(meetingId) {
-    //         const returnedObj = await fetch
-    //             .get(store.serverUrl + `/teacher/meetings/${meetingId}`)
-
-    //         chatArray.current = returnedObj.body.chat;
-    //         transcriptArray.current = returnedObj.body.transcript;
-    //     }
-
-    //     if (meetingObj.chat_url || meetingObj.transcriptArray)
-    //         fetchMeetingDetails(meetingId);
-
-    // }, [chatArray, transcriptArray, meetingId, meetingObj.chat_url, meetingObj.transcriptArray, store.serverUrl])
-
-    // console.log(chatArray)
+    const [loading] = useState(true)
     return useObserver(() => {
-        <p> FUCK YOU!@!!!!!!</p>
+        loading ?
+            <p> FUCK YOU!@!!!!!!</p>
+            :
+            <p> FUCCCCKKKKKK!</p>
+    }
 
-    })
+    )
 }
 
 export default MeetingDetails;
