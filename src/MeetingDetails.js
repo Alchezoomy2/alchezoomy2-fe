248aabd32413875fc0fb0d99ca7613c82056f66e
Paul Stevens <twinkisan@hotmail.com>
2020-12-17 10:24:17 -0800

    debugging

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 // import ReactPlater from "react-player";
 import { useObserver } from 'mobx-react';
 // import { useStateStore } from './StoreProvider.js'
