c0dab23648169be1945158f99fa7dc951fcd87c0
Paul Stevens <twinkisan@hotmail.com>
2020-12-17 14:01:01 -0800

    debugging

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -12,8 +12,7 @@ export const MeetingDetails = (props) => {
     let meetingId = useRef(props.match.params.id)
     // let chatArray = useRef();
     // let transcriptArray = useRef();
-    let meetingObjArray = store.meetingsObj.map(meeting => 
-        if (meeting.id === meetingId) return meeting;
+    let meetingObjArray = store.meetingsObj.map(meeting => { if (meeting.id === meetingId) return meeting; }
     )
 
     console.log(meetingObjArray);
