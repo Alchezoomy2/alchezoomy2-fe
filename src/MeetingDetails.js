ce66ba2a82f0beaeed283887b15fea3b430e1aad
Paul Stevens <twinkisan@hotmail.com>
2020-12-17 12:07:40 -0800

    debugging

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -2,49 +2,26 @@ import React, { useEffect, useState, useRef } from "react";
 // import ReactPlater from "react-player";
 import { useObserver } from 'mobx-react';
 import { useStateStore } from './StoreProvider.js'
-import fetch from 'superagent';
+// import fetch from 'superagent';
 
-const findMeetingObj = (meetingId, meetingsObj) => {
-    for (let meeting of meetingsObj) {
-        if (meeting.id === meetingId) return meeting;
-    }
-}
 
 
 export const MeetingDetails = (props) => {
     const store = useStateStore();
     let [loading, setLoading] = useState(true)
     let meetingId = useRef(props.match.params.id)
-    let chatArray = useRef();
-    let transcriptArray = useRef();
-    let meetingObj = findMeetingObj(meetingId, store.meetingsObj)
+    // let chatArray = useRef();
+    // let transcriptArray = useRef();
 
 
-    console.log('------------------------------------');
-    console.log(`meetingObj:  ${meetingObj}`);
-    console.log('------------------------------------');
     useEffect(() => {
-
-        const fetchMeetingDetails = async (meetingId) => {
-            const returnedObject = await fetch
-                .get(store.serverUrl + `/teacher/meetings/${meetingId.current}`)
-
-            chatArray.current = await returnedObject.body.chat;
-
-            transcriptArray.current = await returnedObject.body.transcript;
-
-            setLoading(false);
-        }
-
-        if (meetingObj.chat_url || meetingObj.transcript_url) fetchMeetingDetails(meetingId);
+        console.log(store.meetingsObj)
+        console.log(store.userType)
     })
 
 
     return useObserver(() =>
         <p>{loading}</p>
-
-
-
     )
 }
 
