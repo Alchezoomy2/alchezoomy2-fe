66fee4d0d075611c8a2d7f151566ec5dfd3821d4
Paul Stevens <twinkisan@hotmail.com>
2020-12-17 11:47:38 -0800

    debugging

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -1,59 +1,59 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
 // import ReactPlater from "react-player";
 import { useObserver } from 'mobx-react';
 import { useStateStore } from './StoreProvider.js'
 // import fetch from 'superagent';
 
-const findMeetingObj = (meetingId, meetingsObj) => {
-    for (let meeting of meetingsObj) {
-        console.log(meeting)
-        if (meeting.id === meetingId) return meeting;
-    }
-}
+// const findMeetingObj = (meetingId, meetingsObj) => {
+//     for (let meeting of meetingsObj) {
+//         console.log(meeting)
+//         if (meeting.id === meetingId) return meeting;
+//     }
+// }
 
 
 export const MeetingDetails = (props) => {
     const store = useStateStore();
     let [loading] = useState(true)
-    let meetingId = useRef(props.match.params.id)
+    // let meetingId = useRef(props.match.params.id)
     // let chatArray = useRef();
     // let transcriptArray = useRef();
 
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        // async function fetchMeetingDetails(meetingId) {
-        //     const returnedObject = await fetch
-        //         .get(store.serverUrl + `/teacher/meetings/${meetingId.current}`)
+    //     // async function fetchMeetingDetails(meetingId) {
+    //     //     const returnedObject = await fetch
+    //     //         .get(store.serverUrl + `/teacher/meetings/${meetingId.current}`)
 
-        //     chatArray.current = await returnedObject.body.chat;
+    //     //     chatArray.current = await returnedObject.body.chat;
 
-        //     transcriptArray.current = await returnedObject.body.transcript;
+    //     //     transcriptArray.current = await returnedObject.body.transcript;
 
-        //     setLoading(false);
+    //     //     setLoading(false);
 
-        //     console.log('------------------------------------');
-        //     console.log(`chatArray[0]:  ${chatArray[0]}`);
-        //     console.log('------------------------------------');
+    //     //     console.log('------------------------------------');
+    //     //     console.log(`chatArray[0]:  ${chatArray[0]}`);
+    //     //     console.log('------------------------------------');
 
-        //     console.log('------------------------------------');
-        //     console.log(`loading:  ${loading}`);
-        //     console.log('------------------------------------');
-        // }
+    //     //     console.log('------------------------------------');
+    //     //     console.log(`loading:  ${loading}`);
+    //     //     console.log('------------------------------------');
+    //     // }
 
-        const meetingsObj = store.meetingsObj;
-        console.log(meetingsObj)
+    //     const meetingsObj = store.meetingsObj;
+    //     console.log(meetingsObj)
 
-        let meetingObj = findMeetingObj(meetingId, meetingsObj)
-
-
-        console.log('------------------------------------');
-        console.log(`meetingObj:  ${meetingObj}`);
-        console.log('------------------------------------');
+    //     let meetingObj = findMeetingObj(meetingId, meetingsObj)
 
 
-        // if (meetingObj.chat_url || meetingObj.transcript_url) fetchMeetingDetails(meetingId);
-    })
+    //     console.log('------------------------------------');
+    //     console.log(`meetingObj:  ${meetingObj}`);
+    //     console.log('------------------------------------');
+
+
+    //     // if (meetingObj.chat_url || meetingObj.transcript_url) fetchMeetingDetails(meetingId);
+    // })
 
 
     return useObserver(() => {
