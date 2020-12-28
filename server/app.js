91587c6c815af85cd458222e155b3a171e42e274
Paul Stevens <twinkisan@hotmail.com>
2020-12-27 20:56:06 -0800

    UGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
     console.log('HI!');
-    res.send('AHHHHHHH!')
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 module.exports = app;
