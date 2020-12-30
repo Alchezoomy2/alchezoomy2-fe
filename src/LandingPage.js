dacd677894cd15365fbf78723b1c3ccf21d094d8
Paul Stevens <twinkisan@hotmail.com>
2020-12-29 17:01:33 -0800

    config server

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,26 +3,24 @@ import { useObserver } from "mobx-react";
 import { useStateStore } from './StoreProvider.js'
 import { Paper, Button } from '@material-ui/core';
 
-export const LandingPage = (props) => {
+export const LandingPage = () => {
     const store = useStateStore();
     console.log(store.serverUrl)
-    console.log(process.env.SERVER_URL)
+    console.log(store.zoomAPIurl)
 
     return useObserver(() =>
         <Paper elevation={3} >
             <Button
                 onClick={() => {
                     store.changeUserType('teacher')
-
-                    window.location.href = "https://zoom.us/oauth/authorize?response_type=code&client_id=AxrbH83_Q0aEO273dFIafw&redirect_uri=http://35.160.21.42/redirect/";
+                    window.location.href = store.zoomAPIurl;
                 }}>
                 Teacher
             </Button>
             <Button
                 onClick={() => {
                     store.changeUserType('student')
-
-                    window.location.href = "https://zoom.us/oauth/authorize?response_type=code&client_id=AxrbH83_Q0aEO273dFIafw&redirect_uri=http://35.160.21.42/redirect/";
+                    window.location.href = store.zoomAPIurl;
                 }}>
                 Student
             </Button>
