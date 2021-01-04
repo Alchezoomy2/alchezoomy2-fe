67d06bf12d1c476418a86bb8081d24176361343f
Paul Stevens <twinkisan@hotmail.com>
2021-01-03 18:49:03 -0800

    monitor video progress

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -12,7 +12,7 @@ export const MeetingDetails = (props) => {
     const store = useStateStore();
     let meetingId = useRef(props.match.params.id)
     const [videoTimestamp, setVideoTimestamp] = useState(0);
-    this.player = React.createRef();
+    let player = React.createRef();
 
 
 
@@ -33,7 +33,7 @@ export const MeetingDetails = (props) => {
 
         function videoProgression() {
             setInterval(() => {
-                console.log(this.player.getCurrentTime())
+                console.log(player.getCurrentTime())
             }, 500)
         }
 
@@ -50,7 +50,7 @@ export const MeetingDetails = (props) => {
                 :
                 <div>
                     <ReactPlayer
-                        ref={this.player}
+                        ref={player}
                         url={`${store.s3VideoUrl}videos/${store.meetingDetails.teacher_id}/${store.meetingDetails.id}.mp4`}
                         controls
                     />
