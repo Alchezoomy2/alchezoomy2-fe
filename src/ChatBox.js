f53c3a6164f61f277635356634abfcf2c8da1221
Paul Stevens <twinkisan@hotmail.com>
2021-01-03 21:49:31 -0800

    monitor video progress

--- src/ChatBox.js
+++ src/ChatBox.js
@@ -116,7 +116,7 @@ export const ChatBox = (props) => {
             setInterval(() => {
                 // store.changeVideoTimestamp(store.chatArray.find(chat => chat.timestamp < store.videoTimestamp))
                 console.log(props.returnVideoTimestamp())
-                console.log(store.chatArray[selectedChatIndex.current]);
+                console.log(store.chatArray[selectedChatIndex.current].timestamp);
                 if (store.chatArray[selectedChatIndex.current + 1].timestamp > props.returnVideoTimestamp()) {
                     selectedChatIndex.current = selectedChatIndex.current + 1;
                 }
