041f28abf2470a5f70e6651fc068fcb7d6944d75
Paul Stevens <twinkisan@hotmail.com>
2021-01-04 21:57:20 -0800

    preloads meetingdetails & adds allows to be started a particular timestamp

--- src/MeetingDetails.js
+++ src/MeetingDetails.js
@@ -17,8 +17,8 @@ export const MeetingDetails = (props) => {
 
     useEffect(() => {
         async function startAtTimestamp() {
-            console.log(parseInt(startingTimestamp))
-            setTimeout(player.current.seekTo(parseInt(startingTimestamp), 'seconds'), 500);
+            console.log(parseInt(startingTimestamp.current))
+            setTimeout(player.current.seekTo(parseInt(startingTimestamp.current), 'seconds'), 500);
         }
         function videoProgression() {
             setInterval(() => {
