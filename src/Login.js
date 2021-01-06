bb9345e7960cd47ba68385997c8693f6ab18b7bb
Paul Stevens <twinkisan@hotmail.com>
2021-01-06 11:29:06 -0800

    adds Login page for preloading login info

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import { useObserver } from 'mobx-react';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useStateStore } from './StoreProvider';
 import { useHistory } from 'react-router-dom';
 import fetch from 'superagent';
@@ -9,33 +9,39 @@ export const Login = async () => {
     const store = useStateStore();
     const history = useHistory();
 
+    useEffect(() => {
 
-    if (store.userType === 'student') {
-        let returnedStudentInfo = await fetch
-            .post(store.serverUrl + '/student/oauth')
-            .send({ code: store.code });
+        async function loginStudent() {
+            let returnedStudentInfo = await fetch
+                .post(store.serverUrl + '/student/oauth')
+                .send({ code: store.code });
 
-        if (returnedStudentInfo.body.new_user) {
-            returnedStudentInfo = await fetch
-                .post(store.serverUrl + '/student/new')
-                .send({ student_info: returnedStudentInfo.body })
+            if (returnedStudentInfo.body.new_user) {
+                returnedStudentInfo = await fetch
+                    .post(store.serverUrl + '/student/new')
+                    .send({ student_info: returnedStudentInfo.body })
+            }
+
+            await store.changeStudentInfo(returnedStudentInfo.body);
+
+            const newMeetingObj = await fetch
+                .post(store.serverUrl + '/student/meetings')
+                .send({ student_info: store.studentInfo })
+
+            store.changeMeetingsObj(newMeetingObj.body);
+
+            const newFavoritesArray = await fetch
+                .get(store.serverUrl + '/student/favorite/' + store.studentInfo.id)
+
+            await store.changeFavoriteArray(newFavoritesArray.body);
+
+            history.push('/student/');
         }
 
-        await store.changeStudentInfo(returnedStudentInfo.body);
-
-        const newMeetingObj = await fetch
-            .post(store.serverUrl + '/student/meetings')
-            .send({ student_info: store.studentInfo })
-
-        store.changeMeetingsObj(newMeetingObj.body);
-
-        const newFavoritesArray = await fetch
-            .get(store.serverUrl + '/student/favorite/' + store.studentInfo.id)
-
-        await store.changeFavoriteArray(newFavoritesArray.body);
-
-        history.push('/student/');
-    }
+        if (store.userType === 'student') {
+            loginStudent();
+        }
+    })
 
     return (
         <p>LOGGING IN!</p>
