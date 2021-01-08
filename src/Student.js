7d2bcc55ec96a49500b99f064f5d2312167a6298
Paul Stevens <twinkisan@hotmail.com>
2021-01-07 19:40:55 -0800

    first step restructuring Student page

--- src/Student.js
+++ src/Student.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography, Grid } from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu';
-import HomeIcon from '@material-ui/icons/Home';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import HomeIcon from '@material-ui/icons/Home';
 // import { useStateStore } from './StoreProvider.js'
 import { useObserver } from 'mobx-react';
 // import classes from '*.module.css';
 import { useHistory } from "react-router-dom";
 import StudentMeetings from "./StudentMeetings.js"
+import StudentHeader from "./StudentHeader.js"
 
 
 
@@ -17,27 +18,7 @@ export const Student = () => {
 
     return useObserver(() =>
         <Grid>
-            <AppBar position="static" style={{ width: '100%' }}>
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6" >
-                        Alchezoomy
-                </Typography>
-                    <IconButton
-                        edge="end"
-                        color="inherit"
-                        aria-label="home"
-                        onClick={() => history.push('/')}>
-                        <HomeIcon />
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
+            <StudentHeader />
             <StudentMeetings />
         </Grid>
 
---
+++ src/StudentHeader.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Grid } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+// import { useStateStore } from './StoreProvider.js'
+import { useObserver } from 'mobx-react';
+// import classes from '*.module.css';
+// import { useHistory } from "react-router-dom";
+
+
+
+export const StudentHeader = () => {
+    // const store = useStateStore();
+    // const history = useHistory();
+
+
+    return useObserver(() =>
+        <AppBar position="static" style={{ width: '100%' }}>
+            <Toolbar>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography
+                    variant="h6" >
+                    Alchezoomy
+                </Typography>
+                <IconButton
+                    edge="end"
+                    color="inherit"
+                    aria-label="home"
+                    onClick={() => history.push('/')}>
+                    <HomeIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+
+export default StudentHeader;
