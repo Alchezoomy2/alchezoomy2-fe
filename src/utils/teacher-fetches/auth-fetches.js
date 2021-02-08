2100a63788da3fa780d90303c2d49e1e43e86b2d
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-07 20:14:05 -0800

    debugging

--- src/utils/teacher-fetches/auth-fetches.js
+++ src/utils/teacher-fetches/auth-fetches.js
@@ -1,9 +1,15 @@
 import fetch from "superagent";
 
 const serverUrl = process.env.REACT_APP_SERVER_URL
+const zoomURL = process.env.REACT_APP_ZOOM_API_URL
+const serverPort = process.env.REACT_APP_SERVER_PORT
+const s3 = process.env.REACT_APP_S3_VIDEO_URL
 
 export async function teacherAuth(code) {
     console.log(serverUrl)
+    console.log(zoomURL)
+    console.log(serverPort)
+    console.log(s3)
     try {
         const response = await fetch
             .post(serverUrl + '/teacher/oauth')
