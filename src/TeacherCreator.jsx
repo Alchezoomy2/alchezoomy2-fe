79fe961e457b138b542e0c75ed3a255002a4ae39
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-12 15:12:47 -0800

    restructure Teacher.js to allow for TeacherCreator.jsx

--- src/TeacherCreator.jsx
+++ src/TeacherCreator.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useStateStore } from './StoreProvider';
-import { Container } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 
 
 export const TeacherCreator = () => {
+    const [teacherInfo, setTeacherInfo] = useState();
     const store = useStateStore()
 
+    useEffect(() => {
+        setTeacherInfo(store.teacherInfo)
+    }, [])
     return (
         <Container>
             <Paper elevation={3} >
-
+                <Typography>
+                    {`WELCOME TO ALCHEMZOOMY ${teacherInfo.user_name}!`}
+                </Typography>
             </Paper>
         </Container>
     )
