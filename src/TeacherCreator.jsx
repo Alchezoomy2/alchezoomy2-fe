3ed790a80692578840920554d8d7b0128a3343d8
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-12 16:25:36 -0800

    restructure Teacher.js to allow for TeacherCreator.jsx

--- src/TeacherCreator.jsx
+++ src/TeacherCreator.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useStateStore } from './StoreProvider';
-import { Container, Typography, Paper } from '@material-ui/core';
+import { Container, Typography, Paper, Avatar } from '@material-ui/core';
 
 
 export const TeacherCreator = () => {
@@ -16,6 +16,24 @@ export const TeacherCreator = () => {
                 <Typography>
                     {`WELCOME TO ALCHEMZOOMY ${teacherInfo.user_name}!`}
                 </Typography>
+                <Typography>
+                    This appears to be your first visit!  You'll need to create an account to continue.  Is this you?
+                </Typography>
+                <Card>
+                    <CardContent>
+                        <Avatar alt={teacherInfo.user_name} src={pic_url} />
+                        <Typography>
+                            {`Name: ${teacherInfo.user_name}`}
+                        </Typography>
+                        <Typography>
+                            {`Email: ${teacherInfo.email}`}
+                        </Typography>
+                        <Typography>
+                            {`Post Color: ${teacherInfo.color}`}
+
+                        </Typography>
+                    </CardContent>
+                </Card>
             </Paper>
         </Container>
     )
