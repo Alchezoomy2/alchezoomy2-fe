e903a7188d8f315ca9feff75f5d6a51395c7b9cd
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-12 17:39:20 -0800

    messing with styling

--- src/TeacherCreator.jsx
+++ src/TeacherCreator.jsx
@@ -6,34 +6,7 @@ import ColorPicker from 'material-ui-color-picker'
 export const TeacherCreator = () => {
     const store = useStateStore()
     const [selectedColor, setSelectedColor] = useState('#FFFFFF')
-    const [textColor, setTextColor] = useState('#000000')
     const [teacherInfo, setTeacherInfo] = useState(store.teacherInfo);
-
-    useEffect(() => {
-
-        function invertColor(hex) {
-            if (hex.indexOf('#') === 0) {
-                hex = hex.slice(1);
-            }
-            // convert 3-digit hex to 6-digits.
-            if (hex.length === 3) {
-                hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-            }
-            if (hex.length !== 6) {
-                throw new Error('Invalid HEX color.');
-            }
-            // invert color components
-            var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
-                g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
-                b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
-            // pad each with zeros and return
-            return '#' + padZero(r) + padZero(g) + padZero(b);
-        }
-
-        console.log(selectedColor)
-        setTextColor(invertColor(selectedColor))
-
-    }, [selectedColor])
 
 
 
@@ -52,7 +25,7 @@ export const TeacherCreator = () => {
                 <Typography>
                     This appears to be your first visit!  You'll need to create an account to continue.  Is this you?
                 </Typography>
-                <Card style={{ height: '800px', backgroundColor: selectedColor, color: textColor }}>
+                <Card style={{ height: '800px', backgroundColor: selectedColor }}>
                     <CardContent>
                         <Avatar alt={teacherInfo.user_name} src={teacherInfo.pic_url} />
                         <Typography>
