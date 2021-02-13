2dc2d98fa088ef89cfd2473b2c530ac13cf1aa3a
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-12 17:04:47 -0800

    building out TeacherCreator

--- src/TeacherCreator.jsx
+++ src/TeacherCreator.jsx
@@ -17,7 +17,7 @@ export const TeacherCreator = () => {
                 <Typography>
                     This appears to be your first visit!  You'll need to create an account to continue.  Is this you?
                 </Typography>
-                <Card style={{ height: '800px', backgroundColor: selectedColor }}>
+                <Card style={{ height: '800px' }}>
                     <CardContent>
                         <Avatar alt={teacherInfo.user_name} src={teacherInfo.pic_url} />
                         <Typography>
