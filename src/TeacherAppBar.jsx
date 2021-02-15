43c6422b8a7277a2159edaf7196cff20e95a0e49
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-15 14:46:48 -0800

    adding teacher Appbar

--- src/TeacherAppBar.jsx
+++ src/TeacherAppBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Avatar, Typography } from "@material-ui/core";
+import { AppBar, Avatar, Typography, Toolbar } from "@material-ui/core";
 import { useStateStore } from "./StoreProvider.js";
 
 
@@ -8,10 +8,16 @@ export const TeacherAppBar = () => {
 
     return (
         <AppBar position="static" style={{ width: "100%", height: "75px" }}>
-            <Avatar alt={store.teacherInfo.userName} src={store.teacherInfo.picUrl} />
-            <Typography>
-                {store.teacherInfo.userName}
-            </Typography>
+            <Toolbar>
+                <Avatar
+                    alt={store.teacherInfo.userName}
+                    src={store.teacherInfo.picUrl}
+                    edge="start"
+                />
+                <Typography>
+                    {store.teacherInfo.userName}
+                </Typography>
+            </Toolbar>
         </AppBar>
     );
 };
