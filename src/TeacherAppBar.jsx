5146a570f945c9f19dfbcc0b396cd952a9a74184
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-15 15:02:29 -0800

    adding teacher Appbar

--- src/TeacherAppBar.jsx
+++ src/TeacherAppBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AppBar, Avatar, Typography, Toolbar, IconButton } from "@material-ui/core";
 import { useStateStore } from "./StoreProvider.js";
 import MailIcon from "@material-ui/icons/Mail";
-import makeStyles from "@material-ui/styles";
+import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles(() => ({
     root: {
