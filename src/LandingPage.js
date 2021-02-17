19ddc7cd88acd0a23ea039ef1ca922cec14d443c
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-17 09:50:12 -0800

    destructuring stlyes

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,14 +2,17 @@ import React from "react";
 import { useObserver } from "mobx-react";
 import { useStateStore } from "./StoreProvider.js";
 import { Paper, Button } from "@material-ui/core";
+import { useStyles } from "./styles/page.js";
 const zoomAPIurl = process.env.REACT_APP_ZOOM_API_URL;
+
 
 
 export const LandingPage = () => {
     const store = useStateStore();
+    const classes = useStyles();
 
     return useObserver(() =>
-        <Paper elevation={3} >
+        <Paper elevation={3} className={classes.root}>
             <Button
                 onClick={() => {
                     store.changeUserType("teacher");
