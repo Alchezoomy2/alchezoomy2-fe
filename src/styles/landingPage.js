8b35f5ada46238f5115cd2846808773b2ce7361f
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-17 10:34:37 -0800

    destructuring stlyes

--- src/styles/landingPage.js
+++ src/styles/landingPage.js
@@ -4,6 +4,7 @@ import { primaryColor, textColor } from "./constants.js";
 export const useStyles = makeStyles({
     root: {
         height: "100vh",
+        flexDirection: "column",
         display: "flex",
         justifyContent: "center",
         alignContent: "center"
@@ -13,6 +14,9 @@ export const useStyles = makeStyles({
         width: "150px",
         backgroundColor: primaryColor,
         color: textColor
+    },
+    mainLogo: {
+        height: "500px",
     }
 });
 
