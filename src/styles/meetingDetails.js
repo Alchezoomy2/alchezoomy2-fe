7f4280e1edd1862188499524734bf0e194cd4a8b
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-18 09:29:38 -0800

    changing chatBox styles

--- src/styles/meetingDetails.js
+++ src/styles/meetingDetails.js
@@ -3,21 +3,20 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
     root: {
         display: "flex",
-        // justifyContent: "center",
+        justifyContent: "center",
         width: "100vw",
         marginTop: "50px",
         alignItems: "center"
     },
     playerWrapper: {
-        width: "1000px",
-        // maxWidth: "800px", // Reset width
+        width: "100%",
+        maxWidth: "800px", // Reset width
         height: "auto", // Reset height
     },
     reactPlayer: {
         // paddingTop: "56.25 %", // Percentage ratio for 16:9
-        position: "absolute",
-        width: "1000px",
-        height: "100%" // Set to relative
+        width: "100%",
+        height: "100%"
     }
 }));
 
