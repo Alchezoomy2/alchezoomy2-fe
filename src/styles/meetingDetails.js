5fad59ca0dc6a77007ddf43352a062cc51d270f7
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-17 18:19:09 -0800

    changing chatBox styles

--- src/styles/meetingDetails.js
+++ src/styles/meetingDetails.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
     root: {
         display: "flex",
-        justifyContent: "space-around",
+        justifyItems: "space-around",
         width: "100vw"
 
     }
