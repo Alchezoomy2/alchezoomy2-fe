8ceb4e358b33a21501628d646145ddd75c4e360a
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-18 09:16:42 -0800

    changing chatBox styles

--- src/styles/meetingDetails.js
+++ src/styles/meetingDetails.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles(() => ({
     },
     reactPlayer: {
         // paddingTop: "56.25 %", // Percentage ratio for 16:9
+        position: "absolute",
         width: "1000px",
         height: "100%" // Set to relative
     }
