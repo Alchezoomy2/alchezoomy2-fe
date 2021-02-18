ad530202a74551dc6a86361af63acaf3aae16afb
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-17 18:19:56 -0800

    changing chatBox styles

--- src/styles/chatbox.js
+++ src/styles/chatbox.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-        width: "75%",
+        width: "60",
     },
     paper: {
         // height: "100%",
