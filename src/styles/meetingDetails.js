ecc4967cedd6bf072bd88033ec16bcf7c9a2958f
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-18 09:14:23 -0800

    changing chatBox styles

--- src/styles/meetingDetails.js
+++ src/styles/meetingDetails.js
@@ -8,13 +8,13 @@ const useStyles = makeStyles(() => ({
         alignItems: "space-around"
     },
     playerWrapper: {
-        width: "90%",
-        maxWidth: "800px", // Reset width
+        width: "1000px",
+        // maxWidth: "800px", // Reset width
         height: "auto", // Reset height
     },
     reactPlayer: {
         // paddingTop: "56.25 %", // Percentage ratio for 16:9
-        width: "100%",
+        width: "1000px",
         height: "100%" // Set to relative
     }
 }));
