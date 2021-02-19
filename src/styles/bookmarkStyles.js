a30930d148450c798e46807e60cc90acca4d05d1
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-18 22:05:40 -0800

    styles bookmarks and favorites

--- src/styles/bookmarkStyles.js
+++ src/styles/bookmarkStyles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
     root: {
         minHeight: "500px",
-        minWidth: "800px",
+        minWidth: "60vw",
         maxHeight: "90vh",
         maxWidth: "80vw",
         overflow: "scroll",
