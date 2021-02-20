f94d2b3100af181d53cf5d79a594bf2797013bea
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-19 18:14:00 -0800

    restyling Teacher

--- src/styles/teacherMeetingItemsStyles.js
+++ src/styles/teacherMeetingItemsStyles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
 
     frame: {
-        borderLeft: "5px solid black"
+        borderTop: "5px solid black"
     }
 }));
 
