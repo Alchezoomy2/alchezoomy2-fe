66186c14228402a1ebdf3821cc6a802182c537f2
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-19 16:34:57 -0800

    restyling Teacher

--- src/styles/teacherDashboardStyles.js
+++ src/styles/teacherDashboardStyles.js
@@ -3,13 +3,15 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
     root: {
         minHeight: "60vh",
-        minWidth: "60vw",
+        minWidth: "500px",
         maxHeight: "90vh",
         maxWidth: "90vw",
         overflow: "scroll",
         marginTop: "25px"
     },
     frame: {
+        width: "100%",
+        height: "100%",
         display: "flex",
         justifyContent: "center"
     }
