7d1971cf3b56f787627ac7a4862632272de5a40f
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-21 11:58:23 -0800

    adds teacher color to student side

--- src/styles/meetingListItemStyles.js
+++ src/styles/meetingListItemStyles.js
@@ -2,7 +2,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(({
     frame: props => ({
-        borderLeft: `15px solid ${props.borderColor}`
+        borderLeft: `15px solid ${props.borderColor}`,
+        padding: "10px"
     })
 }));
 
