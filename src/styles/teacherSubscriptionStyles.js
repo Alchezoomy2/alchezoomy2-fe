d8bf15fb628c8636e854b9dd84cdd8173398a395
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-28 16:16:35 -0800

    tweaking TeacherAppBar

--- src/styles/teacherSubscriptionStyles.js
+++ src/styles/teacherSubscriptionStyles.js
@@ -2,8 +2,17 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
     root: {
-        flexGrow: 1,
-        padding: "5px"
+        minHeight: "60vh",
+        minWidth: "700px",
+        maxHeight: "85vh",
+        width: "70vw",
+        marginTop: "25px",
+        overflowY: "scroll",
+        overflowX: "hidden"
+    },
+    frame: {
+        display: "flex",
+        justifyContent: "center"
     },
     textField: {
         marginLeft: "15px",
