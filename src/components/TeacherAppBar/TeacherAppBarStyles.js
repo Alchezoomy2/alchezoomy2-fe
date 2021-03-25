dda87a856f8e4e3dfcae4a947dfa132794fb853c
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-25 16:32:02 -0700

    seperates Teacher Login from Student Login

--- src/components/TeacherAppBar/TeacherAppBarStyles.js
+++ src/components/TeacherAppBar/TeacherAppBarStyles.js
@@ -1,5 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
-const useStyles = makeStyles(() => ({
+
+export const useStyles = makeStyles(() => ({
     root: {
         flexGrow: 1,
         padding: "5px"
