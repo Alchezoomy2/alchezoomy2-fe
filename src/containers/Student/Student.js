f884b7f4460a6950bdb4646bbede818916c94c88
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-29 17:46:43 -0700

    fixes imports

--- src/containers/Student/Student.js
+++ src/containers/Student/Student.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { Grid } from "@material-ui/core";
 import MeetingDetails from "../../MeetingDetails.js";
 import { useStateStore } from "../../StoreProvider.js";
