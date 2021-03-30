063ca88bc6f5b5703914536b3b257fcc6a430114
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-29 17:44:02 -0700

    fixes imports

---
+++ src/components/StudentLogin/StudentLoginStyles.js
@@ -0,0 +1,33 @@
+import { makeStyles } from "@material-ui/core/styles";
+// import { primaryColor, textColor } from "./constants.js";
+
+export const useStyles = makeStyles({
+    root: {
+        height: "100vh",
+        flexDirection: "column",
+        display: "flex",
+        justifyItems: "center",
+        alignItems: "center"
+    },
+    button: {
+        height: "50px",
+        width: "150px",
+        // backgroundColor: primaryColor,
+        // color: textColor
+    },
+    mainLogo: {
+        height: "500px",
+    },
+    inviteText: {
+        margin: "25px"
+    },
+
+    loginForm: {
+        height: "500px",
+        display: "flex",
+        flexDirection: "column",
+        width: "500px"
+    }
+});
+
+export default useStyles;
