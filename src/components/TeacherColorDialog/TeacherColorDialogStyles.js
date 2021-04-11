8edd2f2d8a7e39655e7f520727af5c1e8a531e30
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-11 12:17:30 -0700

    adds teacher color selector

--- src/components/TeacherColorDialog/TeacherColorDialogStyles.js
+++ src/components/TeacherColorDialog/TeacherColorDialogStyles.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles({
     root: {
         height: "500px",
+        width: "700px",
         flexDirection: "column",
         display: "flex",
         justifyItems: "center",
