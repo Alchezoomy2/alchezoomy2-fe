104e85c18f730ed2ac1c6f345c1eeaeadc1d32cf
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-12 21:53:36 -0700

    debugging

--- src/components/StudentLogin/StudentLogin.jsx
+++ src/components/StudentLogin/StudentLogin.jsx
@@ -19,17 +19,22 @@ export default function StudentLogin() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const studentInfo = await studentAuth(studentEmail.toLowerCase(), password);
-        if (studentInfo.error) {
+        try {
+            if (studentInfo.error) {
+                openSnackbar("error", "Invalid username or password");
+                setPassword("");
+            } else {
+                store.changeStudentInfo(studentInfo);
+
+                const newMeetingObj = await fetchAllStudentMeetings();
+
+                store.changeMeetingsObj(newMeetingObj);
+                store.changeLoggedIn();
+                history.push("/student");
+            }
+        } catch (e) {
             openSnackbar("error", "Invalid username or password");
             setPassword("");
-        } else {
-            store.changeStudentInfo(studentInfo);
-
-            const newMeetingObj = await fetchAllStudentMeetings();
-
-            store.changeMeetingsObj(newMeetingObj);
-            store.changeLoggedIn();
-            history.push("/student");
         }
     };
 
