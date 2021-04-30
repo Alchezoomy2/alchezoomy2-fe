df079bc7d04ee6414a144f61ecac2a6857b1fed0
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-30 14:35:28 -0700

    adds page footer

--- src/components/TeacherSubscriptions/teacherSubscriptionStyles.js
+++ src/components/TeacherSubscriptions/teacherSubscriptionStyles.js
@@ -7,7 +7,8 @@ const useStyles = makeStyles(() => ({
     component: {
         display: "flex",
         flexDirection: "column",
-        width: "100%"
+        width: "100%",
+        minHeight: "85vh"
     },
     textField: {
         marginLeft: "5px",
@@ -21,7 +22,6 @@ const useStyles = makeStyles(() => ({
         marginLeft: "5px"
     },
     searchBar: {
-        // flexGrow: 1,
         width: "100%",
         border: "1px solid #4054b4",
         borderRadius: "5px"
