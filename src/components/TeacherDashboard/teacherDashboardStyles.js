2667960bc4f37fb1f4ba01d4810f139ff4819029
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-30 12:25:46 -0700

    adds page footer

--- src/components/TeacherDashboard/teacherDashboardStyles.js
+++ src/components/TeacherDashboard/teacherDashboardStyles.js
@@ -8,13 +8,13 @@ const useStyles = makeStyles(() => ({
         justifyContent: "center"
     },
     root: {
-        height: "100vh - 50px",
+        height: "100vh",
         width: "100%",
         marginTop: "25px"
     },
     list: {
         width: "95%",
-        height: "100vh - 50px",
+        height: "100vh",
         border: "1px solid green"
     }
 
