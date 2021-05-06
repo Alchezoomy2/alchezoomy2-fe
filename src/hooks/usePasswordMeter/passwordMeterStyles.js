eb2819e2eb7720328e7b4c4cb1bc8a4117454105
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-06 11:48:38 -0700

    add new frame structure to StudentInvite

--- src/hooks/usePasswordMeter/passwordMeterStyles.js
+++ src/hooks/usePasswordMeter/passwordMeterStyles.js
@@ -3,8 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles({
     root: {
-        width: "100%",
-        height: "25px"
+        width: "500px",
+        height: "25px",
+        border: "1px solid yellow"
     },
     progress: {
         WebkitAppearance: "none",
