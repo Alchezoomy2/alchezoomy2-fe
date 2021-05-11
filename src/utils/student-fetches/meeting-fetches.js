1403b68f5175fcdf597d74fdae4eb6dde4e927ce
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-11 15:10:44 -0700

    path is not exact

--- src/utils/student-fetches/meeting-fetches.js
+++ src/utils/student-fetches/meeting-fetches.js
@@ -3,8 +3,8 @@ const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 export async function fetchAllStudentMeetings() {
     const response = await fetch
-        .get(serverUrl + "/student/meetings")
-        .withCredentials();
+        .get(serverUrl + "/student/meetings");
+    // .withCredentials();
 
     return response.body;
 }
