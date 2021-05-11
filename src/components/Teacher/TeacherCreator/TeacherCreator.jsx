aba68dda0bca4beca4e87ba01b4e8606e93ba458
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-10 17:24:13 -0700

    adds jwt checking to teacher invite

--- src/components/Teacher/TeacherCreator/TeacherCreator.jsx
+++ src/components/Teacher/TeacherCreator/TeacherCreator.jsx
@@ -16,7 +16,7 @@ export const TeacherCreator = ({ handleCreateTeacher, creatorOpen }) => {
 
     useEffect(() => {
         if (store.teacherInfo.colorPalette) {
-            console.log("why are we in here?");
+            console.log("test");
             setSelectedColor(store.teacherInfo.colorPalette[0]);
         }
     }, []);
