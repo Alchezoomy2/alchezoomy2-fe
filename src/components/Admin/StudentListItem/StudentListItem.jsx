7c027953eb13c571704e73d73f4c14b7ccd12747
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-12 16:41:40 -0700

    final tweaks

--- src/components/Admin/StudentListItem/StudentListItem.jsx
+++ src/components/Admin/StudentListItem/StudentListItem.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import { ListItem, ListItemText } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
+import { primaryMain } from "../../../styles/colors";
 
 export default function StudentListItem(item, handleItemDelete) {
 
     return (
-        <div>
+        <div
+            style={{ borderLeft: `15px solid ${primaryMain}`, width: "90%", margin: "5px" }}
+        >
             <ListItem
                 alignItems="flex-start"
                 divider={true}
