85630451d3d9a234fa5b291ee5e482871999a9b3
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-12 20:08:55 -0700

    StudentMeetings list height

--- src/components/Shared/Footer/FooterStyles.js
+++ src/components/Shared/Footer/FooterStyles.js
@@ -3,7 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
     root: {
         top: "auto",
-        bottom: 0
+        bottom: 0,
+        position: "absolute"
     },
     frame: {
         display: "flex",
