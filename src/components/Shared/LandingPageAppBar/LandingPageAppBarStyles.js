1bc9721c80c1dac52dd441da861fc8282ac47400
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-05-13 15:38:41 -0700

    restyle StudentInvite

--- src/components/Shared/LandingPageAppBar/LandingPageAppBarStyles.js
+++ src/components/Shared/LandingPageAppBar/LandingPageAppBarStyles.js
@@ -2,7 +2,9 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
     appBar: {
-        width: "100%"
+        width: "100%",
+        padding: "5px",
+        flexGrow: 1,
     },
 }));
 
